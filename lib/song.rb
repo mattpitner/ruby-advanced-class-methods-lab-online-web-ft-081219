307f24fd3b3c2b3016aec083d91d00d30e30e456
Matt Pitner <mattpitner09@gmail.com>
2019-08-26 15:40:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,8 +54,13 @@ class Song
     @@all.sort_by { |song| song.name }
   end 
   
-  def new_from_filename()
-    
+  def new_from_filename(file_name)
+    split_arr = file_name.split(" - ")
+    artist_name = split_arr[0]
+    song_title = split_arr[1].gsub(".mp3", "")
+    song = self.create_by_name(song_title)
+    song.artist_name = artist_name
+    song
   end 
   
   def self.create_from_filename(file_name)
